b4e679172eb1971439c3ad913d784cf15fb0b5c2
Mila <lydmiladerevjanko@gmail.com>
2021-11-21 22:15:23 +0200

    Перебрасывание исключений
    Вызов метода обернут в другие функции, которые при перехвате исключения перебрасывают его(function1) и оборачивают его в непроверяемое исключение(function2)
    
    Продемонстрирован перехват разных исключений с помощью нескольких блоков catch и использование блока finally.(doTask8And9)

--- src/HomeWork19/Main.java
+++ src/HomeWork19/Main.java
@@ -7,13 +7,73 @@ public class Main {
 
     private void run() {
         Student student = new Student();
-        student.setAge(15);
-        student.setName("А");
-
+        student.setAge(27);
+        student.setName("Андрей");
 
         System.out.println("Возраст : " + student.getAge()
-        + "\nИмя " + student.getName());
+                + "\nИмя " + student.getName());
 
+        try {
+            deserialize("строка");
+        } catch (MyDomainException e) {
+            System.out.println("Исключение, положенное в основу текущего исключения. " +
+                    "\nЕсли такое исключение отсутствует," +
+                    " то возвращается пустое значение null: " + e.getCause());
+            System.out.println("Описание исклюсения: " + e.getMessage());
+            System.out.println("Отображение трассы стека: ");
+            e.printStackTrace(System.out);
+        }
+
+        try {
+            function1();
+        } catch (MyDomainException e) {
+            e.printStackTrace();
+        }
+        System.out.println("--------------");
+
+        try {
+            function2();
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        doTask8and9();
+    }
+
+    private void doTask8and9() {
+        try {
+            deserialize("строка");
+
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка " + e.getMessage());
+        } catch (MyDomainException e) {
+            System.out.println("Ошибка " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ошибка " + e.getMessage());
+        } finally {
+            System.out.println("В блоке finally");
+        }
+    }
+
+    private void function2() {
+        try {
+            deserialize("строка");
+        } catch (MyDomainException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+
+    private void function1() throws MyDomainException {
+        try {
+            deserialize("строка");
+
+        } catch (MyDomainException e) {
+            throw e;
+        }
     }
 
     private Student deserialize(String s) throws MyDomainException {
