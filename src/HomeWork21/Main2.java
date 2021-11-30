983cc1ee673349c477f0f8ab8651c5dd11f8b00f
Mila <lydmiladerevjanko@gmail.com>
2021-11-30 22:16:25 +0200

    Реализовано копирование большого файла
    Побайтовое чтение и запись напрямую из/в File[Input/Output]Stream

---
+++ src/HomeWork21/Main2.java
@@ -0,0 +1,34 @@
+package HomeWork21;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) {
+        new Main2().run2();
+    }
+
+    private void run2() {
+        try {
+            readData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void readData() throws IOException {
+        try (InputStream in =
+                     new FileInputStream
+                             ("E:\\Java\\Elementary\\videoplayback.mp4");
+             OutputStream out = new FileOutputStream("E:\\Java\\Elementary\\copy1OfVideo.mp4")) {
+            byte[] chunk = new byte[4096];
+            while (in.available() > 0) {
+                int readCount = in.read(chunk);
+                if (readCount <= 0) {
+                    break;
+                }
+                out.write(chunk, 0, readCount);
+            }
+        }
+    }
+}
