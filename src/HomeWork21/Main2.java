cfe6296098f54cd2752d57cc7f580cf7c4f5e3f4
Mila <lydmiladerevjanko@gmail.com>
2021-12-01 21:50:54 +0200

    Реализовано копирование большого файла
    Побайтовое чтение и запись напрямую из/в File[Input/Output]Stream

--- src/HomeWork21/Main2.java
+++ src/HomeWork21/Main2.java
@@ -9,26 +9,40 @@ public class Main2 {
 
     private void run2() {
         try {
-            readData();
+//            readData();
+            readData2();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    private void readData() throws IOException {
+//    private void readData() throws IOException {
+//        try (InputStream in =
+//                     new FileInputStream
+//                             ("E:\\Java\\Elementary\\videoplayback.mp4");
+//             OutputStream out = new FileOutputStream("E:\\Java\\Elementary\\copy1OfVideo.mp4")) {
+//            byte[] chunk = new byte[4096];
+//            while (in.available() > 0) {
+//                int readCount = in.read(chunk);
+//                if (readCount <= 0) {
+//                    break;
+//                }
+//                out.write(chunk, 0, readCount);
+//            }
+//        }
+//    }
+
+    private void readData2() throws IOException {
         try (InputStream in =
-                     new FileInputStream
-                             ("E:\\Java\\Elementary\\videoplayback.mp4");
-             OutputStream out = new FileOutputStream("E:\\Java\\Elementary\\copy1OfVideo.mp4")) {
-            byte[] chunk = new byte[4096];
+                     new FileInputStream("E:\\Java\\Elementary\\videoplayback.mp4");
+             OutputStream out = new FileOutputStream("E:\\Java\\Elementary\\copy2OfVideo.mp4")) {
             while (in.available() > 0) {
-                int readCount = in.read(chunk);
-                if (readCount <= 0) {
-                    break;
-                }
-                out.write(chunk, 0, readCount);
+                int result = in.read();
+                out.write(result);
             }
         }
     }
+
+
 }
